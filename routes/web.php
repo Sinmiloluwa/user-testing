4fb7f7c9c57d74e2a5b149a27f7ddcad97701a79
Sinmiloluwa <mofeoluwae@gmail.com>
2024-03-26 12:48:56 +0100

    clear cache

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Enums\UserTypeEnum;
 use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +13,9 @@ Route::get('/', function () {
 });
 Route::post('login', [UserController::class, 'login'])->name('login');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; //Return anything
+});
